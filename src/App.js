d78b4d284b0a03abe52e33022eba96515ffa60d0
zeeshana rif <tpc.zeeshan@gmail.com>
2025-02-25 14:44:16 +0530

    header,sidebar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Login from './pages/auth/Login';
+import Sidebar from './common/Sidebar';
+import Header from './common/Header';
 function App() {
   return (
     <div className="App">
@@ -11,6 +13,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/sidebar" element={<Sidebar />} />
+        <Route path="/header" element={<Header />} />
+
+
         {/* Add more routes as needed */}
       </Routes>
     </div>
